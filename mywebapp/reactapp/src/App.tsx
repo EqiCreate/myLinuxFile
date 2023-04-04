197db27e678a85a7c3793e8bd303749c26b0e203
1245994915@qq.com <1245994915@qq.com>
2023-04-05 00:06:51 +0800

    react web app for video

--- mywebapp/reactapp/src/App.tsx
+++ mywebapp/reactapp/src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'video-react/dist/video-react.css'; // import css
+import { Player } from 'video-react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      {/* <header className="App-header"> */}
+        {/* <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
@@ -17,8 +19,11 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
-      </header>
+        </a> */}
+      {/* </header> */}
+      <Player>
+      <source src="https://media.w3.org/2010/05/sintel/trailer_hd.mp4" />
+    </Player>
     </div>
   );
 }
