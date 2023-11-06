3141dc2e7ffef733b57bedb3c93e813ac32bf601
1245994915@qq.com <1245994915@qq.com>
2023-11-06 23:35:53 +0800

    invoke with array

--- myFlutter/my_app/lib/pinvvoke/dll_wrapper.dart
+++ myFlutter/my_app/lib/pinvvoke/dll_wrapper.dart
@@ -1,12 +1,21 @@
 import 'dart:ffi' as ffi;
+import 'dart:ffi';
 import 'dart:io' show Platform, Directory;
-
+import 'dart:typed_data';
+import 'package:ffi/ffi.dart';
 import 'package:path/path.dart' as path;
 
 // FFI signature of the hello_world C function
 typedef HelloWorldFunc = ffi.Void Function();
 // Dart type definition for calling the C foreign function
 typedef HelloWorld = void Function();
+
+typedef SumCFunction = ffi.Int32 Function(ffi.Int32 a, ffi.Int32 b);
+typedef SumDartFunction = int Function(int a, int b);
+
+typedef TransCFunction = ffi.Int8 Function(
+    ffi.Pointer<ffi.Uint8> array, ffi.Int32 len);
+typedef TransDartFunction = int Function(Pointer<ffi.Uint8> array, int len);
 
 void test() {
   // Open the dynamic library
@@ -26,6 +35,21 @@ void test() {
   final dylib = ffi.DynamicLibrary.open(libraryPath);
 
   final hello = dylib.lookupFunction<HelloWorldFunc, HelloWorld>('hello_world');
+  final sum = dylib.lookupFunction<SumCFunction, SumDartFunction>('Sum');
+  final trans =
+      dylib.lookupFunction<TransCFunction, TransDartFunction>('trans');
+
   // Call the function
   hello();
+  print(sum(1, 2));
+
+  final data = Uint8List.fromList([1, 2, 3, 4, 6]);
+
+  // Allocate memory for the data
+  // final dataPtr = data.buffer.asUint8List().elementAt(0);
+  // dataPtr.asTypedList(data.length).setAll(0, data);
+  final dataPtr = calloc<Uint8>(data.length);
+  dataPtr.asTypedList(data.length).setAll(0, data);
+  final result = trans(dataPtr, data.length);
+  calloc.free(dataPtr);
 }
